f8cd0ad202e88baa96012476610252f540ac0cdd
Febby <gunawan.febby@gmail.com>
2014-08-03 02:00:22 +0800

    removed pace asset and updated js with validation

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 //Init variable
 var clientID= "0bb1c26ce5d54ede9f37467517b9286a";
 var url= "https://api.instagram.com/v1/tags/{*}/media/recent"
+
 //Images
 function displayImages(obj){
 	var str = "";
@@ -12,6 +13,12 @@ function displayImages(obj){
 	$('ul.cd-gallery').html(str);
 }
 
+function checkValue(){
+	 if (!$.trim(query)) {
+		alert("error");
+		return false;
+	} 
+}
 
 //search function
 
@@ -19,18 +26,26 @@ $('form').on('submit', function(event) {
 	event.preventDefault();
 	/* Act on the event */
 	var query = $('#search').val();
+
 	$.ajax({
 		url: url.replace('{*}', query) + "?callback=?",
 		type: "GET",
 		dataType: 'json',
 		data: {client_id: clientID
 		},
+		beforeSend: function(){
+			if (!$.trim(query)) {
+		alert("Whoops! Please fill in the search form");
+		return false;
+	} else{$('ul.cd-gallery').html("<div class='loader1'><i></i><i></i></div>")}
+			
+		},
 		success: function (a, b){
 			console.log("success", a.data, b);
 			if (a.data.length){
 				displayImages(a.data);
 			}else {
-				$('.cd-gallery').html("<h3>Sorry, No Results for that query</h3>")
+				$('ul.cd-gallery').html("<h3>Sorry, No Results for that query</h3>")
 			}
 		}
 	})
